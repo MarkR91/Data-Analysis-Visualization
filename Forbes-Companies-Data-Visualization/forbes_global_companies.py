e4f490ed6cb4bb26e679bde1c275a7e3ed31e8b4
MarkR91 <mramdahin@yahoo.com>
2023-08-31 17:18:57 -0400

    Added checks for white spaces in dataframe columns

--- Forbes-Companies-Data-Visualization/forbes_global_companies.py
+++ Forbes-Companies-Data-Visualization/forbes_global_companies.py
@@ -9,19 +9,29 @@ import matplotlib.pyplot as plt
 
 
 # Read in Forbes data set (n<=100)
-df = pd.read_csv('') #rank , global company, country ,sales 	,profit 	,assets	,market value
-
+df = pd.read_csv('forbes_global_2022_companies.csv') # rank , global company, country ,sales 	,profit 	,assets	,market value
+# print("Data type =",type(df))
 
 # Check for missing values
-print(df.isna().to_string())
+# print(df.isna().to_string())
+
+# Print the first 5 rows
+# print(df.head())
          
 # Use dropna() if need be
 
+# Try the following to strip problematic leading and trailing whitespaces that might cause key read errors from each column name.
+df.columns = df.columns.str.strip()
+
+#Check columns
+print('Columns =',df.columns)
+
 # Do a barchart showing total sales for each company in the dataset.
          
-rev_stats = df['sales'].sort_values('',ascending=False)  #sort values
+rev_stats = df.sort_values(by='sales').head() #sort values for first 5 companies
+# print(rev_stats.to_string())
 
-rev_stats[''].plot(kind='bar',rot=0) #create bar chart
+rev_stats.plot(kind='bar',rot=0) #create bar chart
                                           
 
 # Add axis labels and title
@@ -29,28 +39,31 @@ plt.xlabel('Company')
 plt.ylabel('Sales')
 plt.title('Sales by Company')
 
+plt.show()
+
 # Calculate the average profit
-average_profit = df['Profit'].mean()
+# average_profit = df['profit'].mean()
 
 # Create a scatter plot of x verses y
 # plt.scatter(df['x'],df['y'])
 
 # Add axis labels and title
-plt.xlable()
-plt.ylabel()
-plt.title()
+# plt.xlable()
+# plt.ylabel()
+# plt.title()
          
-# Create a series of the countries in the dataframe and sort alphabetically
-# Also, add an index to series created.
+# Create a series of the countries in the dataframe and sort alphabetically. Also, add an index to series created.
 
 # Get the number of countries 
-
 country_counts = df["country"].value_counts() 
 
-# Find the most common country making it into the Forbes listings 
-
+# Find the most common country making it into the Forbes listings.Sort in descending order.
 most_popular_country = country_counts.sort_values(ascending=False).index[0]
+print("most_popular_country =",most_popular_country)
 
+# Find the least common country making it into the Forbes listings 
+least_popular_country = country_counts.sort_values(ascending=True).index[0]
+print("least_popular_country =",least_popular_country)
 
 # Create a new data frame?
 
@@ -59,5 +72,3 @@ most_popular_country = country_counts.sort_values(ascending=False).index[0]
 # Return the default number of rows with head() method.
 
 # Run a correlation on this dataset?
-
-
